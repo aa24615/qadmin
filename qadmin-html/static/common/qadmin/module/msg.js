4a64159e517f2fa14333e96427655c0bf8bd03f8
读心印 <839024615@qq.com>
2020-07-13 11:00:01 +0800

    自动提交_11:00:00

--- qadmin-html/static/common/qadmin/module/msg.js
+++ qadmin-html/static/common/qadmin/module/msg.js
@@ -1,10 +1,33 @@
 
 
-const msg = function () {
-    console.log('msg');
-}
 
 
-export default {
-    msg:msg
-}
+layui.define('layer', function(exports){
+
+    var layer = layui.layer;
+
+    exports('msgs', function () {
+        let msgs = function (code=1,msg='',url='',s=1) {
+            if(typeof code == 'object') { //作为对象传入
+                msg = code.msg;
+                url = code.url || '';
+                s = code.s || 1;
+                code = code.code;
+            }
+            code = code==1 ? 1 : 2;
+            layer.msg(msg, {icon: code,offset: 't',shade: [0.4, '#000']});
+            if(url){
+                setTimeout(function () {
+                    window.location.href = url;
+                },s*1000);
+            }
+        }
+
+        return msgs;
+    });
+
+
+
+})
+
+
--- qadmin-html/static/common/qadmin/qadmin.js
+++ qadmin-html/static/common/qadmin/qadmin.js
@@ -11,52 +11,21 @@
 ;!function(win){
     "use strict";
 
-
     var Qadmin = function(){
         this.v = '1.3'; //版本号
-        this.init = function () {
-            layui.config({
-                base: './static/common/qadmin/module/'
-            }).use(["menu"],function (app) {
-                console.log(app());
-            });
-        }
     }
 
-    Qadmin.prototype.loader  = function (name,callback) {
+    layui.config({
+        base: './static/common/qadmin/module/' //你存放新模块的目录，注意，不是layui的模块目录
+    }).use(['menu','msg'],function (res) {
+        Qadmin.prototype.menu = res;
 
-        var apps = [];
-
-        if(typeof name=='string'){
-            apps.push(name);
-        }
-
-
-        var head = document.getElementsByTagName('head')[0]
-        var node = document.createElement('script');
+        console.log(res);
+    });
 
 
 
-
-        node.async = true;
-        node.charset = 'utf-8';
-        node.src = './static/common/layui/layui.js';
-
-        head.appendChild(node);
-
-        if(node.attachEvent && !(node.attachEvent.toString && node.attachEvent.toString().indexOf('[native code') < 0) && !isOpera){
-            node.attachEvent('onreadystatechange', function(e){
-                onScriptLoad('layui');
-            });
-        } else {
-            node.addEventListener('load', function(e){
-                onScriptLoad('layui');
-            }, false);
-        }
-
-    }
-
-
+    layui.use(['form']);
 
     win.Qadmin = new Qadmin();
 
