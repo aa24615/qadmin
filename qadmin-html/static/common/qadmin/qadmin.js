2edc7f6f1ea1c70cf8701b52ef6e671e104b3572
读心印 <839024615@qq.com>
2020-07-14 09:00:02 +0800

    自动提交_09:00:00

--- qadmin-html/static/common/qadmin/qadmin.js
+++ qadmin-html/static/common/qadmin/qadmin.js
@@ -15,6 +15,15 @@
         this.v = '1.3'; //版本号
     }
 
+
+
+    layui.config({
+        base: './static/common/vue/' //你存放新模块的目录，注意，不是layui的模块目录
+    }).use(['vue.min'],function () {
+
+
+    });
+
     layui.config({
         base: './static/common/qadmin/module/' //你存放新模块的目录，注意，不是layui的模块目录
     }).use(['menu','msgs'],function () {
@@ -24,6 +33,12 @@
         Qadmin.prototype.msg = layui.msg;
         Qadmin.prototype.success = layui.success;
         Qadmin.prototype.error = layui.error;
+
+        Qadmin.prototype.layer = layui.layer;
+        Qadmin.prototype.$ = layui.jquery;
+        Qadmin.prototype.jquery = layui.jquery;
+        win.jquery = layui.jquery;
+        win.$ = layui.jquery;
 
     });
 
